9780cafd978e680b5a0d9aeed48d5ccced48964e
Apriansyah Wahyudi Putra <apriansyahwp844@gmail.com>
2025-01-25 09:32:59 +0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -55,7 +55,7 @@ const About = () => {
 
         <div className='md:w-1/3 lg:w-1/2'>
           <motion.h1 initial={{x:50, opacity:0}} animate={{x:0, opacity:1}} transition={{delay:1.2}} className='text-2xl font-extrabold'>Holla i am {text.split("").map((kata,i) => <motion.span key={i} initial={{opacity:1}} animate={{opacity:0}} transition={{duration:3,repeat:Infinity,delay:i * 0.03}}>{kata}</motion.span>)}</motion.h1>
-          <motion.p initial={{x:50, opacity:0}} animate={{x:0, opacity:1}} transition={{delay:1.5}} className='my-6 font-semibold'>As a programmer with a passion for crafting innovative and efficient digital solutions, I specialize in web development, focusing on delivering user-friendly, scalable results. Let's collaborate and create something extraordinary together!</motion.p>
+          <motion.p initial={{x:50, opacity:0}} animate={{x:0, opacity:1}} transition={{delay:1.5}} className='my-6 font-semibold'>As a programmer with a passion for crafting innovative and efficient digital solutions, I specialize in web development, focusing on delivering user-friendly, scalable results. Let&apos;s collaborate and create something extraordinary together!</motion.p>
           <motion.i initial={{x:50,opacity:0}} animate={{x:0,opacity:1}}>{quotes.split("").map((text,i) => <motion.span key={i} initial={{opacity:0}} animate={{opacity:1,transition:{delay:i * 0.07}}} transition={{repeat:0}} >{text}</motion.span>)}</motion.i>
 
           <motion.h1 initial={{x:50,opacity:0}} animate={{x:0, opacity:1, transition:{delay:1.8}}} className='mt-8 text-2xl font-extrabold mb-6'>Programming Language and etc.</motion.h1>
